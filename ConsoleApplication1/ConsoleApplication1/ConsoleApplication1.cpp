b7e43aec1af31da64eaee3869c6cc57200dd495c
Farbod Hadi Ghanavat <53859071+farbodgh@users.noreply.github.com>
2023-08-23 22:19:05 -0400

    Update ConsoleApplication1.cpp
    final 1

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -23,33 +23,12 @@ int main()
 
 
 	CLI.InitiatingTheProgram(wallet);
-	CLI.CommandHandler(wallet, btc);
 
-	/*
-	std::string commands;
-	BitCoin btc;
-	while (1)
-	{
-		double amount;
-		std::cout << "Please Enter the amount of money you have in your wallet(in usd)\n-> ";
-		std::cin >> amount;
-		Wallet wallet(amount);
-		while (std::cin>> commands)
-		{
+	std::thread cmdreader(&CommandProcessor::CommandReader, &CLI);
+	std::thread cli(&CommandProcessor::CommandHandler, &CLI, std::ref(wallet), std::ref(btc));
+	cli.join();
+	cmdreader.join();
 
-		}
-	}
-	*/
 }
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
