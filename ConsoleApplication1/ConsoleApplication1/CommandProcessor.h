73a37708114d19efa52837fe8a6ba8bab3d7d9cb
Farbod Hadi Ghanavat <53859071+farbodgh@users.noreply.github.com>
2023-08-23 22:18:43 -0400

    Update CommandProcessor.h
    final1

--- ConsoleApplication1/ConsoleApplication1/CommandProcessor.h
+++ ConsoleApplication1/ConsoleApplication1/CommandProcessor.h
@@ -1,16 +1,24 @@
 #pragma once
+#include<mutex>
+#include<thread>
 
 class Wallet;
 class BitCoin;
 
+const char DEFAULT_VALUE = 'p';
+
 class CommandProcessor
 {
 private:
-	char m_command;
-
+	bool m_newInput = true;
+	char m_command = DEFAULT_VALUE;
+	void m_PrintMenue(bool&);
+	void LiveBitcoinPrice(double, Wallet& wallet, BitCoin& btc);
+	std::mutex m_mutexGuard;
 
 public:
-	void CommandHandler(Wallet&, const BitCoin& );
+	void CommandHandler(Wallet&, BitCoin&);
+	void CommandReader();
 	void InitiatingTheProgram(Wallet&);
 };
 
